cba694f192d13034267a42c9311dad94678b9821
Tfauves <tfauvelle@gmail.com>
2021-07-30 01:21:49 -0500

    make bid method logic outlined, needs refactor.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -8,6 +8,8 @@ public class Player {
     public Cup cup;
     public boolean isActivePlayer;
     public Scanner scanner = new Scanner(System.in);
+    public boolean isFirstBid = false;
+    public String betRecord;
 
     public Player() {
         System.out.println("Enter Player Name: ");
@@ -15,13 +17,27 @@ public class Player {
     }
 
     public void makeBid() {
+        while (!isFirstBid) {
         System.out.println("player make your bid :");
         System.out.println("Enter number of dice on table: ");
         int howManyDice = scanner.nextInt();
         System.out.println("Enter face value: ");
         int diceFaceValue = scanner.nextInt();
+        betRecord = "your bet: " + howManyDice + "x " + diceFaceValue;
+        System.out.println(betRecord);
+        isFirstBid = false;
+        }
+//        else {
+//            System.out.println("player make your bid :");
+//            System.out.println("Enter number of dice on table: ");
+//            int howManyDice = scanner.nextInt();
+//            System.out.println("Enter face value: ");
+//            int diceFaceValue = scanner.nextInt();
+//            betRecord = "your bet: " + howManyDice + "x " + diceFaceValue;
+//            System.out.println(betRecord);
+//        }
 
-        System.out.println(bid);
+
     }
 
 
